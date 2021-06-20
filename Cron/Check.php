7b60eb8e7b6530657a3a8d850f48e624618d479a
mark_rees <mark.rees@birdfood.co.uk>
2021-06-20 18:09:55 +0100

    fix for potential issue

--- Cron/Check.php
+++ Cron/Check.php
@@ -5,6 +5,7 @@ namespace ReesSolutions\ElasticStatus\Cron;
 use ReesSolutions\ElasticStatus\Model\Email;
 use ReesSolutions\ElasticStatus\Model\Request;
 use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Framework\ObjectManagerInterface;
 
 class Check
 {
@@ -24,16 +25,26 @@ class Check
     private $scopeConfig;
 
     /**
-     * @param Email $scopeConfig
+     * @var ObjectManagerInterface
+     */
+    protected $_objectManager;
+
+    /**
+     * @param Email $email
+     * @param Request $request
+     * @param ScopeConfigInterface $scopeConfig
+     * @param ObjectManagerInterface $_objectManager
      */
     public function __construct(
         Email $email,
         Request $request,
-        ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig,
+        ObjectManagerInterface $_objectManager
     ) {
         $this->email = $email;
         $this->request = $request;
         $this->scopeConfig = $scopeConfig;
+        $this->_objectManager = $_objectManager;
     }
 
     public function execute()
